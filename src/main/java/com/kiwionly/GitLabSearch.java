22da5c0a83b499ba718105ea07b009ebc6568449
kiwi <gheewooi.ong@seagate.com>
2023-07-22 17:46:28 +0800

    init import

---
+++ src/main/java/com/kiwionly/GitLabSearch.java
@@ -0,0 +1,253 @@
+
+package com.kiwionly;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.gitlab4j.api.Constants.ProjectSearchScope;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.GitLabApiException;
+import org.gitlab4j.api.models.Project;
+import org.gitlab4j.api.models.SearchBlob;
+
+/**
+ * Simple GitLab Search, use async pool to search across gitlab projects content.
+ * <br/><br/>
+ * See Unit Test case for usage
+ */
+public class GitLabSearch {
+
+	private final GitLabApi gitLabApi;
+	private final String url;
+
+	private int poolSize = 10;
+	private boolean verbose = true;
+
+	public GitLabSearch(String token, int timeoutSeconds) {
+		this("https://www.gitlab.com", token, timeoutSeconds);
+	}
+
+	public GitLabSearch(String url, String token, int timeoutSeconds) {
+
+		this.url = url;
+
+		gitLabApi = new GitLabApi(url, token);
+		gitLabApi.setIgnoreCertificateErrors(true);
+		gitLabApi.setRequestTimeout(null, 1000 * timeoutSeconds);
+	}
+
+	private void print(String format, Object... args) {
+		print(false, format, args);
+	}
+
+	private void print(boolean error, String format, Object... args) {
+
+		if(!verbose) {
+			return;
+		}
+
+		if (error) {
+			System.err.printf(format + "\n", args);
+			return;
+		}
+
+		System.out.printf(format + "\n", args);
+	}
+
+	public void setVerbose(boolean verbose) {
+		this.verbose = verbose;
+	}
+
+	public void setPoolSize(int poolSize) {
+		this.poolSize = poolSize;
+	}
+
+	public List<SearchResult> searchByGroupIds(List<Long> groupIds, String query) throws Exception {
+
+		List<Project> projects = new ArrayList<>();
+
+		for (Long id : groupIds) {
+			List<Project> list = gitLabApi.getGroupApi().getProjects(id); // cannot call in async
+			projects.addAll(list);
+		}
+
+		return search(projects, query);
+	}
+
+	public List<SearchResult> searchByProjectId(Long projectId, String query) throws Exception {
+
+		List<Project> projects = List.of(gitLabApi.getProjectApi().getProject(projectId));
+
+		return search(projects, query);
+	}
+
+	public List<SearchResult> searchWithKeyword(String search, String query) throws Exception {
+
+		List<Project> projects = gitLabApi.getProjectApi().getProjects(search);
+
+		return search(projects, query);
+	}
+
+	public List<SearchResult> searchMyProjects(String query) throws Exception {
+
+		List<Project> projects = gitLabApi.getProjectApi().getMemberProjects();
+
+		return search(projects, query);
+	}
+
+	private List<SearchResult> search(List<Project> projects, String query) throws Exception {
+
+		if(query == null || query.trim().equals("")) {
+			throw new IllegalStateException("query cannot be null or empty");
+		}
+
+		if(projects.isEmpty()) {
+			print(true, "No projects found, nothing to search : " + query);
+
+			return new ArrayList<>();
+		}
+
+
+		int len = 30;
+
+		for (Project project : projects) {
+			int length = project.getName().length();
+
+			if(length > len) {
+				len = length + 10;
+			}
+		}
+
+		String pattern = "%-" + len + "s %-10s";
+
+		ExecutorService executor = Executors.newFixedThreadPool(poolSize);
+		List<Future<List<SearchBlob>>> futureList = new ArrayList<>();
+
+		for (Project project : projects) {
+
+			futureList.add(executor.submit(new Callable<>() {
+
+				@Override
+				public List<SearchBlob> call() {
+
+					List<SearchBlob> list = new ArrayList<>();
+
+					String q = query.replace(" ", "%20");
+
+					try {
+
+						long start = System.currentTimeMillis();
+
+						List<?> searchResult = gitLabApi.getSearchApi().projectSearch(project.getId(), ProjectSearchScope.BLOBS, q);
+
+						print(pattern, project.getName(), System.currentTimeMillis() - start);
+
+						for (Object obj : searchResult) {
+							SearchBlob sb = (SearchBlob) obj;
+							list.add(sb);
+						}
+
+						return list;
+
+					} catch (GitLabApiException ex) {
+
+						print(true, "Fail to search project : %-30s url:	%s/api/v4/projects/%s/search?scope=blobs&search=%s",
+								project.getName(), url, project.getId(), q);
+
+						return list;
+					}
+
+				}
+
+			}));
+		}
+
+		print("Search %d projects ...\n", projects.size());
+
+		print(pattern, "Project", "took (ms)");
+		print(pattern, "-------", "---------");
+
+		List<SearchBlob> result = new ArrayList<>();
+
+		for (Future<List<SearchBlob>> future : futureList) {
+			result.addAll(future.get());
+		}
+
+		print("\n");
+
+		List<SearchResult> resultList = new ArrayList<>();
+
+		for (SearchBlob searchBlob : result) {
+
+			for (Project project : projects) {
+
+				if (project.getId().equals(searchBlob.getProjectId())) {
+
+					String name = project.getName();
+					String data = searchBlob.getData();
+					String url = String.format("%s/-/blob/%s/%s", project.getWebUrl(), searchBlob.getRef(), searchBlob.getFilename());
+
+					SearchResult sr = new SearchResult(name, url, data);
+					resultList.add(sr);
+				}
+			}
+		}
+
+		executor.shutdown();
+
+		return resultList;
+	}
+
+	public static class SearchResult {
+
+		private final String name;
+		private final String url;
+		private final String data;
+
+		public SearchResult(String name, String url, String data) {
+			this.name = name;
+			this.url = url;
+			this.data = data;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public String getData() {
+			return data;
+		}
+
+		public String getUrl() {
+			return url.replace(" ", "%20");
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(name);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if ((obj == null) || (getClass() != obj.getClass()))
+				return false;
+			SearchResult other = (SearchResult) obj;
+			return Objects.equals(name, other.name);
+		}
+
+		@Override
+		public String toString() {
+			return "SearchResult [name=" + name + ", url=" + url + ", data=" + data + "]";
+		}
+
+	}
+
+
+}
---
+++ src/test/java/com/kiwionly/GitLabSearchTest.java
@@ -0,0 +1,54 @@
+package com.kiwionly;
+
+import static org.fusesource.jansi.Ansi.ansi;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.kiwionly.GitLabSearch.SearchResult;
+
+class GitLabSearchTest {
+
+	@Test
+	public void testSearch() throws Exception {
+		
+		String url = ""; // your domain url
+		String token = ""; // your gitlab token
+
+		GitLabSearch searcher = new GitLabSearch(url, token, 30); // use longer timeout for larger repository
+		searcher.setVerbose(true); // verbose search info for each project, default true
+		searcher.setPoolSize(10);  // set before call search method, default 10,
+
+		String query = "my search query"; // case insensitive
+
+//		List<SearchResult> list = searcher.searchByGroupIds(List.of(123L, 456L), query);
+//		List<SearchResult> list = searcher.searchByProjectId(1L, query);
+//		List<SearchResult> list = searcher.searchMyProjects(query);
+		List<SearchResult> list = searcher.searchWithKeyword("myproject", query);
+
+		System.out.printf("Found %s results :\n\n", ansi().fgBrightBlue().a(list.size()).reset());
+
+		for (SearchResult res : list) {
+			System.out.printf("project : %s\n", ansi().fgMagenta().a(res.getName()).reset());
+			System.out.printf("url     : %s\n", res.getUrl());
+			System.out.printf("data    : %s\n", ansi().render(getHighlightedData(res.getData(), query)));
+
+			System.out.println();
+		}
+		
+		assertTrue(true);
+		
+	}
+	
+	public static String getHighlightedData(String data, String query) {
+
+		String lower = data.toLowerCase();
+		int index = lower.indexOf(query.toLowerCase());
+		String original =  data.substring(index, index + query.length());
+
+		return data.replaceAll("(?i)" + query, "@|red " + original + "|@");
+	}
+
+}
